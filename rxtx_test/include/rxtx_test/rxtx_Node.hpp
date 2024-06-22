8756442a135e6638fbeff4c2fb8f99e29385dd2e
ZoellickHan <143813528+ZoellickHan@users.noreply.github.com>
2024-06-22 19:43:38 +0800

    compile two pkg with colcon build --symlink-install

--- rxtx_test/include/rxtx_test/rxtx_Node.hpp
+++ rxtx_test/include/rxtx_test/rxtx_Node.hpp
@@ -1,46 +1,31 @@
+#include <rclcpp/logging.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
-// #include <rclcpp/logging.hpp>
-// #include <rclcpp/publisher.hpp>
-// #include <rclcpp/qos.hpp>
-// #include <rclcpp/rclcpp.hpp>
-// #include <rclcpp/subscription.hpp>
-// #include <rclcpp/utilities.hpp>
-// #include <std_msgs/msg/string.hpp>
+#include <rclcpp/subscription.hpp>
+#include <rclcpp/utilities.hpp>
 #include <deque>
 #include "protocol.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include "msg_interfaces/msg/gimbal_msg.hpp"
+#include "msg_interfaces/msg/sentry_gimbal_msg.hpp"
 
 class TestNode : public rclcpp :: Node
 {
 public:
-    TestNode() : Node("test")
-    {
-        while(true)
-        {
-            gimbal_msg_sub_ = create_subscription<serial_driver::TwoCRC_GimbalMsg>( "gimbal_command", rclcpp::SensorDataQoS(),
-            std::bind(debug1, this, std::placeholders::_1));
-
-            sentry_gimbal_msg_sub_ = create_subscription<serial_driver::TwoCRC_SentryGimbalMsg>("sentry_gimbal",rclcpp::SensorDataQoS(),
-            std::bind(debug2,this,std::placeholders::_1));
-
-            chassis_cmd_ = create_publisher<serial_driver::TwoCRC_ChassisCommand>("chassis cmd publish",10);
-            gimbal_cmd_  = create_publisher<serial_driver::TwoCRC_GimbalCommand>("gimbal cmd publish",10); 
-            
-            chassis_cmd_ -> publish(twoCRC_ChassisCommand);
-            gimbal_cmd_ -> publish(twoCRC_GimbalCommand);
-        }
-    }
+    TestNode(const rclcpp::NodeOptions & options);
     ~TestNode();
 
 private:
 
-    void debug1();
+    void debug1(msg_interfaces::msg::GimbalMsg::SharedPtr msg);
 
-    void debug2();
-    // rclcpp::Publisher<serial_driver::TwoCRC_ChassisCommand>::SharedPtr chassis_cmd_;
-    rclcpp::Publisher<auto_aim_interfaces::msg::
-    rclcpp::Publisher<serial_driver::TwoCRC_GimbalCommand>::SharedPtr gimbal_cmd_;
-    rclcpp::Subscription<serial_driver::TwoCRC_SentryGimbalMsg>::SharedPtr sentry_gimbal_msg_sub_;
-    rclcpp::Subscription<serial_driver::TwoCRC_GimbalMsg>::SharedPtr gimbal_msg_sub_;
+    void debug2(msg_interfaces::msg::SentryGimbalMsg::SharedPtr msg);
+    // rclcpp::Publisher<acesserial_driver::TwoCRC_ChassisCommand>::SharedPtr chassis_cmd_;
+    // rclcpp::Publisher<serial_driver::TwoCRC_GimbalCommand>::SharedPtr gimbal_cmd_;
+    rclcpp::Subscription<msg_interfaces::msg::SentryGimbalMsg>::SharedPtr sentry_gimbal_msg_sub_;
+    rclcpp::Subscription<msg_interfaces::msg::GimbalMsg>::SharedPtr gimbal_msg_sub_;
     
     serial_driver::TwoCRC_GimbalCommand   twoCRC_GimbalCommand;
     serial_driver::TwoCRC_ChassisCommand  twoCRC_ChassisCommand;  
--- rxtx_test/src/rxtx_Node.cpp
+++ rxtx_test/src/rxtx_Node.cpp
@@ -1,1 +1,24 @@
+#include "rclcpp/rclcpp.hpp"
 #include "rxtx_test/rxtx_Node.hpp"
+
+TestNode::TestNode(const rclcpp::NodeOptions & options) : Node("rm_serial_driver", options)
+{
+        gimbal_msg_sub_ = this->create_subscription<msg_interfaces::msg::GimbalMsg>( "/gimbal_command", rclcpp::SensorDataQoS(),
+        std::bind(&TestNode::debug1,this,std::placeholders::_1 ));
+
+        sentry_gimbal_msg_sub_ = this->create_subscription<msg_interfaces::msg::SentryGimbalMsg>("/sentry_gimbal",rclcpp::SensorDataQoS(),
+        std::bind(&TestNode::debug2,this, std::placeholders::_1));
+    while(true)
+    {
+    }
+}
+
+void TestNode::debug1(msg_interfaces::msg::GimbalMsg::SharedPtr msg) 
+{
+    return;
+}
+
+void TestNode::debug2( msg_interfaces::msg::SentryGimbalMsg::SharedPtr msg) 
+{
+    return;
+}
