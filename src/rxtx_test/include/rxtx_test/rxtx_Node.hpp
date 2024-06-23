6bb7f9462041689ea81890e08d94cd6c040433fb
ZoellickHan <143813528+ZoellickHan@users.noreply.github.com>
2024-06-23 17:11:09 +0800

    go for dinnar

--- src/rxtx_test/include/rxtx_test/rxtx_Node.hpp
+++ src/rxtx_test/include/rxtx_test/rxtx_Node.hpp
@@ -1,15 +1,18 @@
+//ros
 #include <rclcpp/logging.hpp>
 #include <rclcpp/publisher.hpp>
 #include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/subscription.hpp>
 #include <rclcpp/utilities.hpp>
-#include <deque>
-#include "protocol.hpp"
-#include "rclcpp/rclcpp.hpp"
-
+//msg_interfaces
 #include "msg_interfaces/msg/gimbal_msg.hpp"
 #include "msg_interfaces/msg/sentry_gimbal_msg.hpp"
+#include "msg_interfaces/msg/chassis_command.hpp"
+#include "msg_interfaces/msg/gimbal_command.hpp"
+#include "msg_interfaces/msg/sentry_gimbal_command.hpp"
+//other
+#include "protocol.hpp"
 namespace serial_driver
 {
 class TestNode : public rclcpp::Node
@@ -25,7 +28,11 @@ private:
   
     rclcpp::Subscription<msg_interfaces::msg::SentryGimbalMsg>::SharedPtr sentry_gimbal_msg_sub_;
     rclcpp::Subscription<msg_interfaces::msg::GimbalMsg>::SharedPtr gimbal_msg_sub_;
-    int bag_sum = 0;
+
+    rclcpp::Publisher<msg_interfaces::msg::GimbalCommand>::SharedPtr gimbal_command_pub_;
+    rclcpp::Publisher<msg_interfaces::msg::ChassisCommand>::SharedPtr chassis_command_pub_;
+    rclcpp::Publisher<msg_interfaces::msg::SentryGimbalCommand>::SharedPtr sentry_gimbal_command_pub_;
+    int pkg_sum = 0;
 
 };
 }
--- src/rxtx_test/src/rxtx_Node.cpp
+++ src/rxtx_test/src/rxtx_Node.cpp
@@ -13,28 +13,39 @@ TestNode::TestNode(const rclcpp::NodeOptions & options) : rclcpp::Node("rxtx_tes
     sentry_gimbal_msg_sub_ = this->create_subscription<msg_interfaces::msg::SentryGimbalMsg>("/sentry_gimbal_msg",rclcpp::SensorDataQoS(),
     std::bind(&TestNode::debug2,this, std::placeholders::_1));
 
+    gimbal_command_pub_ = this->create_publisher<msg_interfaces::msg::GimbalCommand>( "/gimbal_commmand",10);
+    chassis_command_pub_ = this->create_publisher<msg_interfaces::msg::ChassisCommand>("/chassis_command",10);
+    sentry_gimbal_command_pub_ = this->create_publisher<msg_interfaces::msg::SentryGimbalCommand>( "/sentry_gimbal_command",10);
+
     RCLCPP_WARN(get_logger(),"Test Node");
+
+    serial_driver::TwoCRC_GimbalCommand TwoCRC_GimbalCommand;
+
+    while(true)
+    {
+        
+    }
 }
 
 void TestNode::debug1(msg_interfaces::msg::GimbalMsg::SharedPtr msg) 
 {
-    this->bag_sum ++;
+    this->pkg_sum ++;
     std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time = end - start;
 
     RCLCPP_WARN(get_logger(),"bagsum = %d , bag rate : %f ,time :%f ,gimbal msg bullet speed = %f \n",
-    bag_sum,double(bag_sum)/time.count(),time.count(),msg->bullet_speed);
+    pkg_sum,double(pkg_sum)/time.count(),time.count(),msg->bullet_speed);
     // printf("gimbal msg bullet speed = %f \n",msg->bullet_speed);
     return;
 }
 
 void TestNode::debug2( msg_interfaces::msg::SentryGimbalMsg::SharedPtr msg) 
 {
-    this->bag_sum ++;
+    this->pkg_sum ++;
     std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> time = end - start;
     RCLCPP_WARN(get_logger(),"bagsum = %d , bag rate : %f ,time :%f ,gimbal msg bullet speed = %f \n",
-    bag_sum,double(bag_sum)/time.count(),time.count(),msg->bullet_speed);
+    pkg_sum,double(pkg_sum)/time.count(),time.count(),msg->bullet_speed);
     RCLCPP_WARN(get_logger(),"gimbal msg bullet speed = %f \n",msg->bullet_speed);
     return;
 }
